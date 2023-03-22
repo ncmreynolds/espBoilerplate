90dfcac805f37686457409f3cf446cffe65f665e
Nick Reynolds <ncmreynolds@googlemail.com>
2023-03-22 17:06:55 +0000

    Specific time server and timezone example

---
+++ examples/specifiedTimeServerAndZone/specifiedTimeServerAndZone.ino
@@ -0,0 +1,32 @@
+#include <espBoilerplate.h>
+#include <credentials.h>  //Used to contain my own personal Wi-Fi credentials but not pushed to GitHub, comment this line out and use your own below
+#ifndef WIFI_SSID
+  #define WIFI_SSID "test"
+  #define WIFI_PSK "testpresharedkey"
+#endif
+
+bool connectedOk = false; //Store the state of the connection
+struct tm currentTime;  //Store the current time
+
+void setup() {
+  Serial.begin(115200);                                         //Set up the Serial device
+  espBoilerplate.setOutputStream(Serial);                       //Optionally you can specifiy the Stream to send output to, eg. if using Serial1
+  espBoilerplate.setRetries(60);                                //Optionally, increase retries on the connection to 60s. Default is 30s.
+  espBoilerplate.configureNtp("pool.ntp.org");                  //Optionally, enable NTP for a specific time server
+  espBoilerplate.configureTimeZone("GMT0BST,M3.5.0/1,M10.5.0"); //Optionally, configure the specific timezone in IANA format, see https://raw.githubusercontent.com/nayarsystems/posix_tz_db/master/zones.csv for the list
+  connectedOk = espBoilerplate.begin(WIFI_SSID,WIFI_PSK); //Connect to the Wi-Fi SSID WIFI_SSID with pre-shared key WIFI_PSK, return is true if succesful
+  if(connectedOk == false)
+  {
+    Serial.println("Restarting");
+    ESP.restart();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(getLocalTime(&currentTime))
+  {
+    Serial.printf("Current time: %02u:%02u:%02u %02u/%02u/%02u\r\n", currentTime.tm_hour, currentTime.tm_min, currentTime.tm_sec, currentTime.tm_mday, currentTime.tm_mon +1, currentTime.tm_year + 1900);
+  }
+  delay(1000);
+}
